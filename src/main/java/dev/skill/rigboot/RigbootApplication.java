d7b28fb628b7792d092026a4a015a94bb43a68fc
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2025-02-09 13:45:51 -0300

    Initial project

---
+++ src/main/java/dev/skill/rigboot/RigbootApplication.java
@@ -0,0 +1,13 @@
+package dev.skill.rigboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RigbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RigbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/skill/rigboot/RigbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skill.rigboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RigbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
